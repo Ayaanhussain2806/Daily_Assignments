e010eecb7362ed66fe293e5b08647b616b81731e
Mohd Ayaan Hussain <142593144+Ayaanhussain2806@users.noreply.github.com>
2025-09-30 23:59:10 +0530

    Add files via upload

---
+++ 567. Permutation in String.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean checkInclusion(String s1, String s2) {
+        int n=s1.length();
+        int m=s2.length();
+        char[] chars = s1.toCharArray();
+        Arrays.sort(chars);
+        String sorted = new String(chars);
+        for(int i=0;i<=m-n;i++){
+            String sub=s2.substring(i,i+n);
+            char[] cha = sub.toCharArray();
+            Arrays.sort(cha);
+            String s = new String(cha);
+            if(sorted.equals(s)){
+                return true;
+            }
+        }
+        return false;
+    }
+}
