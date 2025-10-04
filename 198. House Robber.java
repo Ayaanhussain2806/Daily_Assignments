225a424d559713b9ce6f06059888bfec5c3b9206
Mohd Ayaan Hussain <142593144+Ayaanhussain2806@users.noreply.github.com>
2025-10-04 23:18:55 +0530

    Add files via upload

---
+++ 198. House Robber.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int rob(int[] nums) {
+        int l=nums.length;
+        if(l==0) return 0;
+        if(l==1) return nums[0];
+        int dp[]=new int[l];
+        dp[0]=nums[0];
+        dp[1]=Math.max(nums[0],nums[1]);
+        for(int i=2;i<l;i++){
+            int max=nums[i]+dp[i-2];
+            dp[i]=Math.max(max,dp[i-1]);
+        }
+        return dp[l-1];
+    }
+}
