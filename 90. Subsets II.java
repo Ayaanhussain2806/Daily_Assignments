d72d4a1174c4d66288a8a96600db720b357e6b3c
Mohd Ayaan Hussain <142593144+Ayaanhussain2806@users.noreply.github.com>
2025-10-04 13:38:47 +0530

    Add files via upload

---
+++ 90. Subsets II.java
@@ -0,0 +1,20 @@
+class Solution {
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+        Arrays.sort(nums);
+        List<List<Integer>> ans=new ArrayList<>();
+        List<Integer> ll=new ArrayList<>();
+        solve(nums,ans,0,ll);
+        return ans;
+    }
+    public static void solve(int[] nums, List<List<Integer>> ans,int idx,List<Integer> ll){
+        ans.add(new ArrayList<>(ll));
+        for(int i=idx;i<nums.length;i++){
+            if(i>idx && nums[i]==nums[i-1]){
+                continue;
+            }
+            ll.add(nums[i]);
+            solve(nums,ans,i+1,ll);
+            ll.remove(ll.size()-1);
+        }
+    }
+}
